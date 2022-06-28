2ae9865dcb7253be0a449743f727370e0876a6b3
Bob Kozdemba <bkozdemba@gmail.com>
2022-06-27 19:03:47 -0500

    updated

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,8 +3,13 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+# BK
+import pandas as pd
+from ml.data import process_data
 
 # Add code to load in the data.
+# BK
+data = pd.read_csv('../data/census.csv') 
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
