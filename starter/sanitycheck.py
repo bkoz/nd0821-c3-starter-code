c6d11e2dbc2b73c35a3c01b394c7c9bd132dafbf
Bob Kozdemba <bkozdemba@gmail.com>
2022-06-29 17:28:16 -0500

    formatting

--- starter/sanitycheck.py
+++ starter/sanitycheck.py
@@ -3,18 +3,22 @@ from os import path
 import argparse
 import importlib
 import inspect
-import os
+# import os
 import sys
 
 FAIL_COLOR = '\033[91m'
 OK_COLOR = '\033[92m'
 WARN_COLOR = '\033[93m'
 
+
 def run_sanity_check(test_dir):
 
-    #assert path.isdir(test_dir), FAIL_COLOR+f"No direcotry named {test_dir} found in {os.getcwd()}"
-    print('This script will perform a sanity test to ensure your code meets the criteria in the rubric.\n')
-    print('Please enter the path to the file that contains your test cases for the GET() and POST() methods')
+    # assert path.isdir(test_dir),
+    # FAIL_COLOR+f"No direcotry named {test_dir} found in {os.getcwd()}"
+    print('This script will perform a sanity test to ensure your code meets the \
+    criteria in the rubric.\n')
+    print('Please enter the path to the file that contains your test cases for the \
+        GET() and POST() methods')
     print('The path should be something like abc/def/test_xyz.py')
     filepath = input('> ')
 
@@ -24,18 +28,27 @@ def run_sanity_check(test_dir):
     module_name = path.splitext(path.basename(filepath))[0]
     module = importlib.import_module(module_name)
 
+    test_function_names = list(filter(lambda x:
+                               inspect.isfunction(getattr(module, x)) and
+                               not x.startswith('__'), dir(module))
+                               )
 
-    test_function_names = list(filter(lambda x: inspect.isfunction(getattr(module,x)) and not x.startswith('__'), dir(module)))
-
-    test_functions_for_get = list(filter(lambda x: inspect.getsource(getattr(module,x)).find('.get(') != -1 , test_function_names))
-    test_functions_for_post = list(filter(lambda x: inspect.getsource(getattr(module,x)).find('.post(') != -1, test_function_names))
-    
+    test_functions_for_get = list(filter(lambda x:
+                                  inspect.getsource(getattr(module, x)).
+                                  find('.get(') != -1,
+                                  test_function_names)
+                                  )
+    test_functions_for_post = list(filter(lambda x:
+                                   inspect.getsource(getattr(module, x)).
+                                   find('.post(') != -1,
+                                   test_function_names)
+                                   )
 
     print("\n============= Sanity Check Report ===========")
     SANITY_TEST_PASSING = True
     WARNING_COUNT = 1
 
-    ## GET()
+    # GET()
     TEST_FOR_GET_METHOD_RESPONSE_CODE = False
     TEST_FOR_GET_METHOD_RESPONSE_BODY = False
     if not test_functions_for_get:
@@ -43,17 +56,18 @@ def run_sanity_check(test_dir):
         WARNING_COUNT += 1
         print(FAIL_COLOR+"No test cases were detected for the GET() method.")
         print(FAIL_COLOR+"\nPlease make sure you have a test case for the GET method.\
-            This MUST test both the status code as well as the contents of the request object.\n")
+            This MUST test both the status code as well as the contents of \
+            the request object.\n")
         SANITY_TEST_PASSING = False
 
     else:
         for func in test_functions_for_get:
-            source = inspect.getsource(getattr(module,func))
+            source = inspect.getsource(getattr(module, func))
             if source.find('.status_code') != -1:
                 TEST_FOR_GET_METHOD_RESPONSE_CODE = True
-            if (source.find('.json') != -1) or (source.find('json.loads') != -1):
-                TEST_FOR_GET_METHOD_RESPONSE_BODY =  True
-
+            if (source.find('.json') != -1) or \
+               (source.find('json.loads') != -1):
+                TEST_FOR_GET_METHOD_RESPONSE_BODY = True
 
         if not TEST_FOR_GET_METHOD_RESPONSE_CODE:
             print(FAIL_COLOR+f"[{WARNING_COUNT}]")
@@ -99,18 +113,21 @@ def run_sanity_check(test_dir):
         if not TEST_FOR_POST_METHOD_RESPONSE_CODE:
             print(FAIL_COLOR+f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+"One or more of your test cases for POST() do not seem to be testing the response code.\n")
+            print(FAIL_COLOR+"One or more of your test cases for POST() do \
+                not seem to be testing the response code.\n")
         if not TEST_FOR_POST_METHOD_RESPONSE_BODY:
             print(FAIL_COLOR+f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+"One or more of your test cases for POST() do not seem to be testing the contents of the response.\n")
+            print(FAIL_COLOR+"One or more of your test cases for POST() do \
+                not seem to be testing the contents of the response.\n")
 
-        if len(test_functions_for_post) >= 2 and COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT < 2:
-            print(FAIL_COLOR+f"[{WARNING_COUNT}]")
-            WARNING_COUNT += 1
-            print(FAIL_COLOR+"You do not seem to have TWO separate test cases, one for each possible prediction that your model can make.")
-
-
+        if len(test_functions_for_post) >= 2 \
+            and COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT < 2:
+                print(FAIL_COLOR+f"[{WARNING_COUNT}]")
+                WARNING_COUNT += 1
+                print(FAIL_COLOR+"You do not seem to have TWO separate test \
+                    cases, one for each possible prediction that your model \
+                        can make.")
 
     SANITY_TEST_PASSING = SANITY_TEST_PASSING and\
         TEST_FOR_GET_METHOD_RESPONSE_CODE and \
@@ -121,15 +138,19 @@ def run_sanity_check(test_dir):
 
     if SANITY_TEST_PASSING:
         print(OK_COLOR+"Your test cases look good!")
-    
-    print(WARN_COLOR+"This is a heuristic based sanity testing and cannot guarantee the correctness of your code.")
-    print(WARN_COLOR+"You should still check your work against the rubric to ensure you meet the criteria.")
 
+    print(WARN_COLOR+"This is a heuristic based sanity testing and cannot \
+    guarantee the correctness of your code.")
+    print(WARN_COLOR+"You should still check your work against the rubric \
+    to ensure you meet the criteria.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('test_dir',metavar='test_dir',nargs='?',default='tests',help='Name of the directory that has test files.')
+    parser.add_argument(
+        'test_dir', metavar='test_dir', nargs='?',
+        default='tests',
+        help='Name of the directory that has test files.'
+        )
     args = parser.parse_args()
     run_sanity_check(args.test_dir)
-
