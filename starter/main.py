5dfc106f21e7141a5ad7d76a02e2aa04eeddc7bf
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-09 14:29:50 -0500

    added Census schema

--- starter/main.py
+++ starter/main.py
@@ -2,6 +2,7 @@
 # BK
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic import Field
 from typing import Union, List
 import joblib
 import json
@@ -19,6 +20,25 @@ class TaggedItem(BaseModel):
     tags: Union[str, List[str]]
     item_id: int
 
+
+class CensusRequest(BaseModel):
+    age: int = 1
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int = Field(None, alias='education-num')
+    marital_status: str = Field(None, alias='marital-status')
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int = Field(None, alias='capital-gain')
+    capital_loss: int = Field(None, alias='capital-loss')
+    hours_per_week: int = Field(40, alias='hours-per-week')
+    native_country: str = Field(None, alias='native-country')
+    salary: str
+
+logging.info(f"CensusRequest = {CensusRequest.schema_json(indent=2)}")
 
 model_file = 'starter/model/model.pkl'
 model = joblib.load(model_file)
@@ -52,3 +72,10 @@ async def predict2(body: List[List[int]]) -> str:
     logging.info(f"model_server: r = {r}")
     # return "PREDICT"
     return json.dumps(r.tolist())
+
+@app.post("/predict3/")
+async def predict3(body: CensusRequest) -> dict:
+    logging.info(f"model_server: body = {body}")
+    # r = model.predict(body)
+    return "PREDICT3"
+
