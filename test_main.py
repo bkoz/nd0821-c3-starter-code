960e01f779543e02eb873e1a69d139ac202c0ab8
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-11 14:48:35 -0500

    screen shots

--- test_main.py
+++ test_main.py
@@ -50,16 +50,6 @@ def test_post_0():
     logging.info(f"status code = {response.status_code}")
     logging.info(response.json())
 
-    # logging.info("test: Loading payload.")
-    # payload_file = 'starter/data/payload.json'
-    # f = open(payload_file)
-    # payload_data = json.load(f)
-    # logging.info(f"test: Loaded payload data = {payload_data}.")
-    # response = client.post("/predict/", data=json.dumps(payload_data))
-    # logging.info(f"test: Prediction: {response.json()}")
-    # print(f"test: Prediction: {response.json()}")
-    # assert 200, response.status_code == 200
-
 
 def test_post_1():
     #
